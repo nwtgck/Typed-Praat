7b9c1ac12fdb84e92fdff40a72042de9b28639b2
OtaRyo <nwtgck@gmail.com>
2016-07-10 09:43:29 +0900

    [rename]

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -4,7 +4,7 @@ import scala.reflect.runtime.universe._
 /**
   * Created by Jimmy on 2016/07/08.
   */
-object Main2 {
+object Main {
   def main(args: Array[String]) {
 
     val fileContent = Source.fromFile("./src/main/scala/testScripts/Assignment4.scala")
